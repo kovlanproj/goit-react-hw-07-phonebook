53a0f4a39ca823f5ae84ca8025a03d651288dac9
kovlanproj <kozorizva@gmail.com>
2022-09-26 16:38:08 +0300

    add slice

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,40 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const contactsApi = createApi({
+  reducerPath: 'contactsApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://63316e67cff0e7bf70ed0435.mockapi.io',
+  }),
+  tagTypes: ['Contacts'],
+  endpoints: builder => ({
+    fetchContacts: builder.query({
+      query: () => '/contacts',
+      providesTags: ['Contacts'],
+    }),
+
+    deleteContact: builder.mutation({
+      query: contactId => ({
+        url: `/contacts/${contactId}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Contacts'],
+    }),
+    addContact: builder.mutation({
+      query: body => {
+        return {
+          url: '/contacts',
+          method: 'POST',
+          body,
+        };
+      },
+
+      invalidatesTags: ['Contacts'],
+    }),
+  }),
+});
+
+export const {
+  useFetchContactsQuery,
+  useDeleteContactMutation,
+  useAddContactMutation,
+} = contactsApi;
